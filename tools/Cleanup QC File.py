582f2f1a6009a0464f8608792b1ae2a76a515ddb
notkoen <45914779+notkoen@users.noreply.github.com>
2023-04-05 15:15:30 -0700

    Add cleanup QC file script

---
+++ tools/Cleanup QC File.py
@@ -0,0 +1,20 @@
+import os
+
+current_dir = os.getcwd()
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def process_file(file_path):
+    output_lines = []
+    with open(file_path, 'r') as input_file:
+        for line in input_file:
+            if not line.startswith('$cbox') and not line.startswith('$bbox') and not line.startswith('$illumposition'):
+                output_lines.append(line)
+    with open(file_path, 'w') as output_file:
+        output_file.writelines(output_lines)
+
+for root, dirs, files in os.walk(os.getcwd()):
+    for file in files:
+        if file.endswith('.qc'):
+            file_path = os.path.join(root, file)
+            process_file(file_path)
